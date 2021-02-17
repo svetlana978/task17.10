8b5623121e42d5d5b4ba89f68a47319930b1f480
Svetlana <cbeta279@gmail.com>
2021-02-17 13:12:41 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+class Family extends Shooter
+{
+  public $peopleCount = 3;
+  public $surname = 'Smith';
+  public $address = 'Moscow';
+
+  public function loveFamilyMembers()
+  {
+    //...
+  }
+}
+
+class Father extends Family
+{
+  public $wife = 'Alice';
+  public $age = 35;
+  
+  public function careForChildren()
+  {
+    //...
+  }
+
+  public function earnMoney()
+  {
+    //...
+  }
+}
+
+class Mother extends Family
+{
+  public $husband = 'John';
+  public $age = 33;
+  
+  public function careForChildren()
+  {
+    //...
+  }
+
+  public function provideComfort()
+  {
+    //...
+  }
+}
+
+class Child extends Family
+{
+  public $sex = 'male';
+  public $age = 11;
+  
+  public function study()
+  {
+    //...
+  }
+
+  public function helpParents()
+  {
+    //...
+  }  
+}
+
+?>
